99284e3cd85059cf8793394b586c184f6ec86016
Nithish2702 <nithishniki27@gmail.com>
2026-05-04 23:36:51 +0530

    Updated backend code

--- Test1/backend/build.sh
+++ Test1/backend/build.sh
@@ -2,5 +2,9 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies for psycopg2
+apt-get update
+apt-get install -y libpq-dev gcc
+
 pip install --upgrade pip
 pip install -r requirements.txt
